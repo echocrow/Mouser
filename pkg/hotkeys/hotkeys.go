9dc8b010de6b2d2b89d2841b861c5bc0352c82dd
Crow <5488190+echocrow@users.noreply.github.com>
2021-01-08 00:13:32 +0100

    Prep hotkey package

---
+++ pkg/hotkeys/hotkeys.go
@@ -0,0 +1,4 @@
+// Package hotkeys provides functionality to create hotkeys and monitor their
+// events. Hotkeys are registered keyboard buttons that trigger an action.
+// Hotkey events include key-down and key-up events.
+package hotkeys

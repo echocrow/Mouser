58b28ec850d46e7cf29f285054a2e5ddad3efb24
Crow <5488190+echocrow@users.noreply.github.com>
2021-01-23 13:47:08 +0100

    Simplify hotkey monitor init

--- pkg/hotkeys/hotkeys.go
+++ pkg/hotkeys/hotkeys.go
@@ -2,3 +2,39 @@
 // events. Hotkeys are registered keyboard buttons that trigger an action.
 // Hotkey events include key-down and key-up events.
 package hotkeys
+
+import (
+	"sync"
+
+	"github.com/birdkid/mouser/pkg/hotkeys/hotkey"
+	"github.com/birdkid/mouser/pkg/hotkeys/monitor"
+	"github.com/birdkid/mouser/pkg/log"
+)
+
+var (
+	sharedMonitor     *monitor.Monitor
+	sharedMonitorOnce sync.Once
+)
+
+// DefaultMonitor constructs a shared default hotkey monitor.
+func DefaultMonitor(initWithLog bool) *monitor.Monitor {
+	sharedMonitorOnce.Do(func() {
+		sharedMonitor = defaultMonitor(initWithLog)
+	})
+	return sharedMonitor
+}
+
+// defaultMonitor constructs a default hotkey monitor.
+func defaultMonitor(withLog bool) *monitor.Monitor {
+	hkReg := defaultRegistry()
+	monitor := monitor.New(hkReg, nil)
+	if withLog {
+		monitor.SetLogCb(log.NewCallback("Monitor"))
+	}
+	return monitor
+}
+
+// DefaultRegistry constructs a default hotkey registry.
+func defaultRegistry() hotkey.Registry {
+	return hotkey.NewRegistry(nil, nil)
+}
